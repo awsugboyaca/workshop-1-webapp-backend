2f9c396497df0b0cedeebd8649acee24e0c7970c
David Vargas <dradav.97@gmail.com>
2024-01-27 21:05:53 -0500

    feat: running

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,32 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'mi-bucket-strapi.s3.us-east-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'mi-bucket-strapi.s3.us-east-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
